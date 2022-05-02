2582493a7ef3ce8f1c191a23e2a6172ea6dca225
charlottehuynh8510 <charlottehuynh8510@gmail.com>
2022-05-02 22:24:01 +1000

    [Charlotte - debug for signup]

--- frontend/src/Screens/signupScreen/signupScreen.js
+++ frontend/src/Screens/signupScreen/signupScreen.js
@@ -14,37 +14,40 @@ export function SignUpScreen(){
   
   const Signup = (details, repeatPassword) => {
       console.log(details);
-
-      if (details.password !== repeatPassword){
-            console.log("unmatched password")
-            setError("Unmatched password. Please try again!")
-        //   setUser({
-        //       username: details.username,
-        //       email: details.email,
-        //       password: details.password
-        //   })
-      } else {
-            console.log("Matched password");
-            axiosInstance
-                .post('user/create/', {
-                    email: details.email,
-                    user_name: details.username,
-                    password: details.password,
-                }) 
-                .then((res) => {
-                    history('/login')
-                    console.log(res.data)
-                    console.log(res)
-      }).catch((err) => {
-          if (err.response.data[0] === 'This username already exists'){
-              setError('This username already exists. Please try again!')
-          } else if (err.response.status === 500){
-              setError('This email already exists. Please try again!')
-          } else{
-              console.err(err)
-          }
-      })
-    }
+      if ((details.email === '') || (details.username === '') || (details.password === '')){
+        setError('Please fill in the required details')
+      }
+      else{
+        if (details.password !== repeatPassword){
+                console.log("unmatched password")
+                setError("Unmatched password. Please try again!")
+            //   setUser({
+            //       username: details.username,
+            //       email: details.email,
+            //       password: details.password
+            //   })
+        } else {
+                console.log("Matched password");
+                axiosInstance
+                    .post('user/create/', {
+                        email: details.email,
+                        user_name: details.username,
+                        password: details.password,
+                    }) 
+                    .then((res) => {
+                        history('/login')
+                        console.log(res.data)
+                        console.log(res)
+                }).catch((err) => {
+                    if (err.response.data[0] === 'This username already exists'){
+                        setError('This username already exists. Please try again!')
+                    } else if (err.response.status === 500){
+                        setError('This email already exists. Please try again!')
+                    } else{
+                        console.err(err)
+                }
+            })
+    }}
   }
 
   return(

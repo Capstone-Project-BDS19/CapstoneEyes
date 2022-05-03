51438fe9488addd9fe9309dbc079a458494110b0
Hoang Minh Quan Le <minhquanlh@gmail.com>
2022-05-03 11:44:59 +1000

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'capstone',
-    'capstone_api',
     'rest_framework',
+    'capstone',
+    'capstone_api',  
     'corsheaders',
     'users'
 ]
